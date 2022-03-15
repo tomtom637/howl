323c4483328b996dff3dbd28dd4c805974267e67
Thomas Lacombe <tomtom637@gmail.com>
2022-03-15 13:00:48 +0100

    refactored and added forms for authentication

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import LogoBg from './components/LogoBg/LogoBg';
-//import './icons.css';
+
+import { atom, useAtom } from 'jotai';
+import { tokenAtom, userInfosAtom, formAtom, loggedAtom } from './store';
+
+import { getInfosFromToken } from './api-calls';
+
 import GlobalStyles from './global-styles/GlobalStyles';
+
 import Header from './patterns/Header/Header';
 import Hero from './patterns/Hero/Hero';
+import { LoginForm, SignupForm } from './components/Forms/Forms';
 import Footer from './components/Footer/Footer';
 
-export default function App() {  
+const App = () => {
+  const [token, setToken] = useAtom(tokenAtom);
+  const [userInfos, setUserInfos] = useAtom(userInfosAtom);
+  const [formType, setFormType] = useAtom(formAtom);
+  const [logged, setLogged] = useAtom(loggedAtom);
+
+  useEffect(() => {
+    if (token) {
+      getInfosFromToken(userInfos, setUserInfos, token, setLogged);
+    } else {
+      setLogged(false);
+    }
+  }, []);
+
+  const { nickname, email, motto, picture, role } = userInfos ?? {};
   return (
     <>
       <GlobalStyles />
       <div className="site-wrapper">
-        <Header />
-        <Hero />
+        <div className='main-content-wrapper'>
+          <Header />
+          <Hero />
+          {!logged && (
+            formType === 'login'
+              ? <LoginForm />
+              : <SignupForm />
+          )}
+        </div>
         <Footer />
       </div>
     </>
   );
-}
+};
+
+export default App;

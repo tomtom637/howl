d2398b3d93dde709536be797d530172f5b387e19
Thomas Lacombe <tomtom637@gmail.com>
2022-03-21 13:45:08 +0100

    added the about page

--- front/src/components/About/About.js
+++ front/src/components/About/About.js
@@ -4,7 +4,7 @@ import { device } from '../../device';
 export default function About({ toggledMenu }) {
   return (
     <div className="container">
-      <h1>About</h1>
+      <h1 className='page-title'>About</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A minima excepturi dolorem nihil natus amet id neque atque. Veritatis libero beatae voluptate nihil eum eius excepturi assumenda ducimus saepe modi!</p>
     </div>
   );

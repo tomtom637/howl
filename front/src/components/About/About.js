d457d40919aaf5cd24beb260bade721f2187fd08
Thomas Lacombe <tomtom637@gmail.com>
2022-04-06 14:41:24 +0200

    added link to home page in the about page

--- front/src/components/About/About.js
+++ front/src/components/About/About.js
@@ -1,11 +1,20 @@
 import '../../icons.css';
 import { device } from '../../device';
+import {Link} from 'react-router-dom';
 
 export default function About({ toggledMenu }) {
   return (
     <div className="container">
       <h1 className='page-title'>About</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A minima excepturi dolorem nihil natus amet id neque atque. Veritatis libero beatae voluptate nihil eum eius excepturi assumenda ducimus saepe modi!</p>
+      <p>
+        <Link
+          style={{
+            color: 'var(--secondary)',
+            fontSize: '1.2rem',
+          }}
+          to="/">Back to the homepage</Link>
+      </p>
     </div>
   );
 }
